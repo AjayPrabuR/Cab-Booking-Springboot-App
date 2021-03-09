fb6b74e1fd5a88c235dc1ac5ddf8b016042a785c
Ajay Prabu R <ajayprabu.rajasekar@thoughtworks.com>
2021-03-09 22:15:16 +0530

    [Ajay] | added Location.java file

---
+++ src/main/java/com/example/cabbooking/model/Location.java
@@ -0,0 +1,25 @@
+package com.example.cabbooking.model;
+
+public class Location {
+    private int x;
+    private int y;
+
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Location() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+}
