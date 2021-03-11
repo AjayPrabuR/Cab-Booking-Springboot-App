4edc364e96162e74b1347c40c9a1b634ab1821a0
Ajay Prabu R <ajayprabu.rajasekar@thoughtworks.com>
2021-03-11 21:33:41 +0530

    [Ajay] | updated end-points

--- src/main/java/com/example/cabbooking/controller/CabFacilityController.java
+++ src/main/java/com/example/cabbooking/controller/CabFacilityController.java
@@ -6,35 +6,33 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("cab")
 public class CabFacilityController {
-    private Cab[] cabs = null;
+    private List<Cab> cabs ;
+    public CabFacilityController() {
+        this.cabs = new ArrayList<>();
+    }
 
     private int getDistanceTravelled(Location pickupLocation, Location destinationLocation) {
         int distanceTravelled = (int)Math.sqrt((destinationLocation.getX() - pickupLocation.getX()) * (destinationLocation.getX() - pickupLocation.getX())  +  (destinationLocation.getY() - pickupLocation.getY()) * (destinationLocation.getY() - pickupLocation.getY()));
         return distanceTravelled;
     }
 
-    @PutMapping("/register")
-    public ResponseEntity<String> register() {
-        cabs = new Cab[5];
-        for (int i = 0; i < cabs.length; i++) {
-            cabs[i] = new Cab(i + 1);
-        }
-
-        if (cabs == null) return new ResponseEntity(HttpStatus.BAD_REQUEST);
-
-        return new ResponseEntity(HttpStatus.OK);
+    @PostMapping("/register/{cabType}")
+    public ResponseEntity register(@PathVariable String cabType) {
+        int cabRegisterNumber = cabs.size() + 1;
+        cabs.add(new Cab(cabRegisterNumber));
+       // System.out.print(cabType);
+        return ResponseEntity.ok(cabRegisterNumber);
     }
 
     @GetMapping("/status")
     public ResponseEntity<String> status() {
-        if (cabs == null) {
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
-        }
         StringBuilder buildResponse = new StringBuilder();
         for (Cab cab : cabs) {
             buildResponse.append(cab.toString()+"\n");
@@ -42,17 +40,15 @@ public class CabFacilityController {
         return new ResponseEntity(buildResponse.toString(), HttpStatus.OK);
     }
 
-    @PutMapping("/book/")
+    @PutMapping("/book")
     public ResponseEntity<String> book(@RequestBody Map<String, Location> booking) {
-        if (cabs == null) {
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
-        }
+        //System.out.print(">>>>>>"+cabs.size()+">>>>>>>\n");
         Location customerPickupLocation = booking.get("pickupLocation");
         Location customerDropLocation = booking.get("destinationLocation");
         String bill= "";
         int nextCabAvailable = -1;
-        for (int i = 0; i < cabs.length; i++) {
-            if (!cabs[i].isBooked()) {
+        for (int i = 0; i < cabs.size(); i++) {
+            if (!cabs.get(i).isBooked()) {
                 {
                     nextCabAvailable = i;
                     break;
@@ -62,15 +58,15 @@ public class CabFacilityController {
 
 
         if (nextCabAvailable != -1) {
-            cabs[nextCabAvailable].setBooked(true);
-            cabs[nextCabAvailable].settimeTaken(getDistanceTravelled(customerPickupLocation, customerDropLocation));
-            cabs[nextCabAvailable].setPickupLocation(customerPickupLocation);
-            cabs[nextCabAvailable].setDestinationLocation(customerDropLocation);
-            bill=  "Cab_number " + String.valueOf(cabs[nextCabAvailable].getCabNumber()) + ",\nTime Taken: " + String.valueOf(cabs[nextCabAvailable].gettimeTaken()) +  ",\nFair: " + cabs[nextCabAvailable].gettimeTaken() * 5  +"\n";
+            cabs.get(nextCabAvailable).setBooked(true);
+            cabs.get(nextCabAvailable).settimeTaken(getDistanceTravelled(customerPickupLocation, customerDropLocation));
+            cabs.get(nextCabAvailable).setPickupLocation(customerPickupLocation);
+            cabs.get(nextCabAvailable).setDestinationLocation(customerDropLocation);
+            bill=  "Cab_number " + String.valueOf(cabs.get(nextCabAvailable).getCabNumber()) + ",\nTime Taken: " + String.valueOf(cabs.get(nextCabAvailable).gettimeTaken()) +  ",\nFair: " + cabs.get(nextCabAvailable).gettimeTaken() * 5  +"\n";
             return new ResponseEntity(bill, HttpStatus.OK);
         }
 
-        return new ResponseEntity(bill, HttpStatus.OK);
+        return new ResponseEntity(bill, HttpStatus.NOT_FOUND);
     }
 
 }
--- src/test/java/com/example/cabbooking/controller/CabFacilityControllerTest.java
+++ src/test/java/com/example/cabbooking/controller/CabFacilityControllerTest.java
@@ -1,63 +1,46 @@
 package com.example.cabbooking.controller;
 
+import com.example.cabbooking.model.Location;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.hamcrest.Matchers.containsString;
+import java.util.HashMap;
+import java.util.Map;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
 
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(CabFacilityController.class)
 class CabFacilityControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
     @MockBean
     private CabFacilityController cabFacilityController;
 
     @Test
-    public void shouldReturn200WhenRegisteringAllCabs() throws Exception {
-        ResponseEntity response = new ResponseEntity(HttpStatus.OK);
-        when(cabFacilityController.register()).thenReturn(response);
-        this.mockMvc.perform(put("/cab/register")).andExpect(status().isOk());
+    public void shouldReturn200AndCabRegisterNumberWhenRegisteringACab() {
+        ResponseEntity response = ResponseEntity.ok(1);
+        when(cabFacilityController.register("small")).thenReturn(response);
     }
 
     @Test
-    public void shouldReturn400WhenCabsAreNotRegistered() throws Exception {
-        ResponseEntity response = new ResponseEntity(HttpStatus.BAD_REQUEST);
+    public void shouldReturn200WhenCabsAreRegistered() {
+        cabFacilityController.register("small");
+        ResponseEntity response = new ResponseEntity(HttpStatus.OK);
         when(cabFacilityController.status()).thenReturn(response);
-        this.mockMvc.perform(get("/cab/status")).andExpect(status().isBadRequest());
     }
 
-
     @Test
-    public void shouldReturn200WhenBookingACabAfterRegistering() throws Exception {
+    public void shouldReturn200WhenBookingACabAfterRegistering() {
+        Map<String , Location> requestBody = new HashMap<>();
+        requestBody.put("pickupLocation" , new Location(-4,6));
+        requestBody.put("destinationLocation", new Location(4,6));
         ResponseEntity response = new ResponseEntity(HttpStatus.OK);
-        when(cabFacilityController.register()).thenReturn(response);
-        this.mockMvc.perform(put("/cab/register")).andExpect(status().isOk());
-
-        ResponseEntity response_Check = new ResponseEntity("{ pickupLocation: {x:-4,y: 6}, destinationLocation: { x:4 , y:6}}", HttpStatus.OK);
-
-        mockMvc.perform(
-                put("/cab/book/")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{ \"pickupLocation\" :{ \"x\" :-4, \"y\" :6}, \"destinationLocation\": { \"x\":4, \"y\":6} }"));
-
-        when(cabFacilityController.status()).thenReturn(response_Check);
-        this.mockMvc.perform(get("/cab/status/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("{ pickupLocation: {x:-4,y: 6}, destinationLocation: { x:4 , y:6}}")));
+        when(cabFacilityController.book(requestBody)).thenReturn(response);
     }
 
 }
